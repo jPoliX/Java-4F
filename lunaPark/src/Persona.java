cf039ee601d4e44c8cb99126022f51920501d1e6
alessiopoli <150335994+jPoliX@users.noreply.github.com>
2024-11-07 13:23:14 +0100

    Update Persona.java

--- lunaPark/src/Persona.java
+++ lunaPark/src/Persona.java
@@ -1,17 +1,15 @@
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 public class Persona {
-    public int biglietto;
-    public LocalDateTime entrata = LocalDateTime.now();
-    public giostre giostreUsate;
+    public int nBiglietto;
+    public LocalDateTime momentoEvento;
+    public ListaGiostre giostreProvate;
 
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm:ss");
-        return "Biglietto: " + biglietto + " Orario Entrata: " + entrata.format(formatter);
+        return "Biglietto: " + nBiglietto + " Orario: " + momentoEvento.format(formatter);
     }
 
 }
