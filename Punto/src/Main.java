f2874d6bf54817183c7129481f76e77b4f0ab98c
alessiopoli <150335994+jPoliX@users.noreply.github.com>
2024-11-25 09:21:06 +0100

    Update Main.java

--- Punto/src/Main.java
+++ Punto/src/Main.java
@@ -4,10 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         try {
-            Punto p = new Punto(2, 3);
-            Rettangolo r = new Rettangolo(p, p);
+            Punto p = new Punto(5, 3);
+            Punto p2 = new Punto(2,4);
+            Rettangolo r = new Rettangolo(p, p2);
+            Rettangolo r2 = new Rettangolo(p, p2, "messi");
             System.out.println("Perimetro: " + r.calcolaPerimetro());
             System.out.println("Area: " + r.calcolaArea());
+            System.out.println(r.toString());
+            System.out.println(r2.toString());
         } catch (Exception e) {
             System.out.println("Errore: " + e.getMessage());
         }
