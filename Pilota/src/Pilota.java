658b8aa1a9bbe163af26bbb5dc05c1aca8e0338d
alessiopoli <150335994+jPoliX@users.noreply.github.com>
2024-12-09 09:04:57 +0100

    Update Pilota.java

--- Pilota/src/Pilota.java
+++ Pilota/src/Pilota.java
@@ -1,35 +1,38 @@
 import java.util.ArrayList;
 
 public class Pilota {
-    private String nome, cognome;
-    private ArrayList<Pilota> scuderia;
-    public Pilota(String nome, String cognome) throws Exception {
-        if(nome.equals(null) && cognome.equals(null)) {
-            throw new Exception("Nome e cognome sono nulli");
+    private String nome, cognome, nazionalita;
+    public Pilota(String nome, String cognome, String nazionalita) throws Exception{
+        if(isValido(nome) && isValido(cognome) && isValido(nazionalita)){
+            this.nome = nome;
+            this.cognome = cognome;
+            this.nazionalita = nazionalita;
+        }else {
+            throw new Exception("Valori non validi");
         }
-        this.nome = nome;
-        this.cognome = cognome;
     }
     public String getNome() {
         return nome;
     }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
     public String getCognome() {
         return cognome;
     }
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
+    public String getNazionalita() {
+        return nazionalita;
     }
-    public ArrayList<Pilota> getScuderia() {
-        return scuderia;
+    private boolean isValido(String str){
+        return str != null && !str.isEmpty();
     }
-    public void setScuderia(ArrayList<Pilota> scuderia) {
-        this.scuderia = scuderia;
-    }
+
     @Override
     public String toString() {
-        return "Pilota" + "nome: %s" + nome  + ", cognome: %s" + cognome + ", scuderia: %s" + scuderia;
+        return "Pilota:" + " nome: " + nome  + ", cognome: " + cognome;
+    }
+    @Override
+    public boolean equals(Object obj){
+        if(obj != null || obj.getClass() != this.getClass()){
+            return false;
+        }
+        return this.nome.equals(((Pilota) obj).nome) && this.cognome.equals(((Pilota) obj).cognome) && this.nazionalita.equals(((Pilota) obj).nazionalita);
     }
 }
