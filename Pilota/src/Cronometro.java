d600c87b0156f3945e0f5c06e14bd34dd49d5e1f
alessiopoli <150335994+jPoliX@users.noreply.github.com>
2024-12-09 09:23:07 +0100

    Add files via upload

---
+++ Pilota/src/Cronometro.java
@@ -0,0 +1,34 @@
+import java.time.LocalTime;
+
+public class Cronometro {
+    private LocalTime startTime, endTime;
+    private int tempoGiro;
+    public Cronometro() {
+        setStartTime();
+        setEndTime();
+    }
+    public void setStartTime(){
+        startTime = LocalTime.now();
+    }
+    public void setEndTime(){
+        endTime = LocalTime.now();
+    }
+
+    public int getTempoGiro() {
+        return tempoGiro;
+    }
+
+    public void calcolaIntTimer() throws Exception{
+        int tempoGiro;
+        tempoGiro = this.endTime.toSecondOfDay() - this.startTime.toSecondOfDay();
+        if(tempoGiro == 0){
+            throw new Exception("Il cronomtro è fermo");
+        }
+        this.tempoGiro = tempoGiro;
+
+    }
+    @Override
+    public String toString(){
+        return String.format("TEMPO: %d", tempoGiro);
+    }
+}
