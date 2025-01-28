13763877ed0ed350d9c653c534cf29d5e3067b7e
alessiopoli <150335994+jPoliX@users.noreply.github.com>
2025-01-28 12:27:30 +0100

    Add files via upload

---
+++ martellollo/src/Automobile.java
@@ -0,0 +1,17 @@
+public class Automobile extends Veicolo {
+    private int nPorte;
+
+    public Automobile(String marca, String modello, double prezzo, int nPorte) {
+        super(marca, modello, prezzo);
+        this.nPorte = nPorte;
+    }
+
+    public int getnPorte() {
+        return nPorte;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(super.toString() + "Numero Porte: %d", nPorte);
+    }
+}
---
+++ martellollo/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Veicolo v1 = new Veicolo("Ferrari", "F90", 5);
+        Automobile a1 = new Automobile("Ferrari", "F40", 5, 2);
+        Moto m1 = new Moto("Moto", "Brum", 10, 29);
+        Concessionaria c = new Concessionaria();
+
+    }
+}
