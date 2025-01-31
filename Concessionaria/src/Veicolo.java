8a7b30d3c4c664200a88ebae70ed6742934bc594
alessiopoli <150335994+jPoliX@users.noreply.github.com>
2025-01-31 22:21:50 +0100

    Add files via upload

---
+++ Concessionaria/src/Veicolo.java
@@ -0,0 +1,38 @@
+public class Veicolo {
+    private String marca;
+    private String modello;
+    private double prezzo;
+
+    public Veicolo(String marca, double prezzo, String modello) {
+        this.marca = marca;
+        this.prezzo = prezzo;
+        this.modello = modello;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+    @Override
+    public String toString(){
+        return String.format("%s - %s - %f", marca, modello, prezzo);
+    }
+    @Override
+    public boolean equals(Object o){
+        if(o instanceof Veicolo){
+            return this.marca.equals(((Veicolo)o).marca) && this.modello.equals(((Veicolo)o).modello);
+        }
+        return false;
+    }
+}
